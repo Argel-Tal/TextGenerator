2683fdb5f3fd2517126bf24176890b20b572c9b1
Jack <80669114+Argel-Tal@users.noreply.github.com>
2021-05-21 13:50:11 +1200

    Comments added and code cleaned up

--- Lab10/SRNTrain.py
+++ Lab10/SRNTrain.py
@@ -2,23 +2,25 @@ from COSC343WordEnc import COSC343WordEnc
 from COSC343SRNClassifier import COSC343SRNClassifier
 import pickle , gzip
 
+# input file
 with open("Marx.txt", "r") as f:
     all_text = f.read()
 
+# hyper params
 batchN = 100
 lr = 0.001
 hiddenSize = 100
 epochs = 50
 
-
+# data set up as one-hot-encoding
 word_enc = COSC343WordEnc(max_text_length=100000)
 X = word_enc.fit_transform(all_text)
 
+# training of model
 print(str(epochs) + " epochs")
 marxClass = COSC343SRNClassifier(hidden_layer_size = hiddenSize, activation = 'relu', solver='adam', batch_size = batchN, learning_rate_init = lr, max_iter=epochs, verbose=True, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
-
 marxClass.fit(X[:-1], X[1:])
 
-
+# saving model file
 with gzip.open("srn\_training.save", 'w') as f:
     pickle.dump((marxClass, word_enc), f)
--- Lab10/loadingModel.py
+++ Lab10/loadingModel.py
@@ -1,23 +1,24 @@
 import pickle, gzip
-
 from COSC343WordEnc import COSC343WordEnc
 
+# set up file for write out of generated text
 with gzip.open("srn\_training.save") as f:
     marxClass, word_enc = pickle.load(f)
 
-
+# prompt set up
 primer = "fellows, my friends"
 allText = ""
 marxClass.reset()
 
+# data into and out of generative model pt1
 primerEncoded = word_enc.transform(primer)
-
 output = marxClass.predict(primerEncoded)
-
 decoded = word_enc.inverse_transform(output)
+# output management
 print(decoded)
 allText = primer + " " + decoded
 
+# data into and out of generative model pt2 and output management
 for i in range(50):
     primer = decoded
     primerEncoded = word_enc.transform(primer)
@@ -26,6 +27,7 @@ for i in range(50):
     print(decoded)
     allText = allText + " " + decoded
 
+# write out of generated text 
 with open("marxOut5.txt", 'w') as g:
     g.write(allText)
     g.close()
